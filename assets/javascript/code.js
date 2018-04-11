fcfdd329476e4aea3ead2b6572a9b7013a6b3db9
Matthew Buchanan <matt@matthews-air.home>
2018-04-11 14:42:32 -0400

    train schedule outputs to bootstrap card and updates every minute using moment.js

--- assets/javascript/code.js
+++ assets/javascript/code.js
@@ -14,6 +14,33 @@ $('#submit').click(function(event) {
 });
 
 database.ref().on('value', function(snap) {
-  console.log(JSON.stringify(snap.val()));
+  //console.log(JSON.stringify(snap.val()));
+  $('#trains').empty();
+  snap.child('trains').forEach(function(childSnap) {
+	  //console.log(childSnap.val());
+	  var timeLeft= getMinutesTilTrain(childSnap.val().start, childSnap.val().frequency);
+	  var nextTime = moment().add(timeLeft, "minutes").format('hh:mm A');
+	  $('#trains').append(`
+	  <tr>
+	    <td>${childSnap.val().name}</td>
+		<td>${childSnap.val().destination}</td>
+		<td>${childSnap.val().frequency}</td>
+		<td>${nextTime}</td>
+		<td>${timeLeft}</td>  
+	  </tr>
+	  `);  
+  });
 });
+
+function getMinutesTilTrain(firstTime, tFrequency) {
+	var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
+    var currentTime = moment();
+    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+    var tRemainder = diffTime % tFrequency;
+   
+    return (tFrequency - tRemainder);
+
+}
+
+
 
