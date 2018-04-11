42f4f44851304e022aaa5e55db9f82c95f1fc03b
Matthew Buchanan <matt@matthews-air.home>
2018-04-11 16:27:20 -0400

    form validation

--- assets/javascript/code.js
+++ assets/javascript/code.js
@@ -8,9 +8,28 @@ setInterval(function() {
 }, 1*1000);
 
 $('#submit').click(function(event) {
+	var regex = new RegExp('^[0-1][0-9]:[0-5][0-9]$|^[2][0-3]:[0-5][0-9]$');
+	//console.log($('#start').val().match(regex));
+	var fieldsFilled = true;
+	var inputs = document.getElementsByClassName('add');
+	for (var i = 0; i < inputs.length; i++) {
+		if(!(inputs[i].validity.valid)) {
+			inputs[i].setAttribute('class', 'form-control add is-invalid');
+			fieldsFilled = false;
+		}
+	}
     event.preventDefault();
-    database.ref().child('trains').push({'name': $('#name').val(), 'destination': $('#destination').val(), 'start':  $('#start').val(), 'frequency': $('#frequency').val()});
-	$('.add').val('');
+	if (Number.parseInt($('#frequency').val(), 10) === NaN || Number.parseInt($('#frequency').val(), 10) < 1) {	
+		$('#frequency').val('Enter an integer greater than or equal to 1');	
+	}
+	else if ($('#start').val().match(regex) === null) {
+		$('#start').val('Enter a time between 00:00 and 23:59');
+	}
+	else if (fieldsFilled) {
+    	database.ref().child('trains').push({'name': $('#name').val(), 'destination': $('#destination').val(), 'start': $('#start').val(), 'frequency': $('#frequency').val()});
+		$('.add').val('');
+		$('.add').attr('class', 'form-control add');
+    }
 });
 
 database.ref().on('value', function(snap) {
